3052dfa453f2a2f68b723772e79eb9cd7639f2d6
Matthew Taylor <majikpig@gmail.com>
2020-09-20 20:42:16 -0400

    nearly complete

--- src/App.js
+++ src/App.js
@@ -2,73 +2,10 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import {Cell,World} from './classes/Cell.js';
 import styled from 'styled-components';
+import {info} from './info.js';
 
-const Board = styled.div`
-  max-width: 90vw;
-  max-height: 90vw;
-`
+const c = ['cell-overlay ','alive ','transition200 ','transition400 ','transition600 '];
 
-const DeadCell = styled.div`
-  display: flex;
-  flex-direction: column;
-  border: 1px dotted green;
-  height: 1vw;
-  width: 1vw;
-  margin: -1px;
-`
-
-const LiveCell = styled.div`
-  background-color: lightgreen;
-  border: 1px solid green;
-  border-radius: 20%;
-  display: flex;
-  flex-direction: column;
-  height: 1vw;
-  width: 1vw;
-  margin: -1px;
-`
-// margin: 1px;
-
-const CellRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  height: 1vw;
-  margin: 2px;
-`
-
-const NumberInput = styled.input`
-  width: 30px;
-`
-
-const ButtonBox = styled.div`
-  margin: 4px;
-  display: flex;
-  flex-direction: row;
-`
-
-const Label = styled.span`
-  margin-left: 5px;
-  margin-right: 5px;
-  color: green;
-`
-
-const ActiveLabel = styled.span`
-  margin-left: 5px;
-  margin-right: 5px;
-  color: lightgreen;
-`
-
-const Icon = styled.i`
-  margin-left: 5px;
-  margin-right: 5px;
-  color: green;
-`
-
-const ActiveIcon = styled.i`
-  margin-left: 5px;
-  margin-right: 5px;
-  color: lightgreen;
-`
 
 function App() {
   const [world, setWorld] = useState(new World(25,25));
@@ -76,8 +13,12 @@ function App() {
   const [ref,setRef] = useState(0);
   const [isActive, setIsActive] = useState(false);
   const [seconds, setSeconds] = useState(0);
-  const [speed, setSpeed] = useState(1000);
+  const [speed, setSpeed] = useState(500);
   const [size, setSize] = useState(2);
+  const [showInfo, setShowInfo] = useState(false);
+  const [message, setMessage] = useState(0);
+  const [cellClass, setCellClass] = useState(c[0]+c[3]);
+  const [cellAliveClass, setCellAliveClass] = useState(c[0]+c[1]+c[3]);
 
   useEffect(()=>{
     if (world) {
@@ -85,7 +26,21 @@ function App() {
     }
   },[])
 
+  useEffect(()=>{
+    if (speed===250) {
+      setCellClass(c[0]+c[2]);
+      setCellAliveClass(c[0]+c[1]+c[2]);
+    } else if (speed===500) {
+      setCellClass(c[0]+c[3]);
+      setCellAliveClass(c[0]+c[1]+c[3]);
+    } else {
+      setCellClass(c[0]+c[4]);
+      setCellAliveClass(c[0]+c[1]+c[4]);
+    }
+  },[speed])
+
   const random = () => {
+    setMessage(1);
     world.randomize();
     setRef(ref+1);
   }
@@ -101,6 +56,7 @@ function App() {
   }
 
   const reset = () => {
+    setMessage(0);
     setIsActive(false);
     setSeconds(0);
     world.reset();
@@ -121,6 +77,11 @@ function App() {
   }, [isActive, seconds, step, speed]);
 
   const toggle = () => {
+    if (message>1) {
+      setMessage(1);
+    } else {
+      setMessage(2);
+    }
     setIsActive(!isActive);
   }
 
@@ -133,8 +94,11 @@ function App() {
 
   const flip = (row,col) => {
     if (!isActive) {
+      setMessage(1);
       world.flip(row,col);
       setRef(ref+1);
+      // document.getElementById(`Cell-${row}-${col}`).style.transform = "rotateY(180deg)";
+      // setRef(ref+1);
     }
   }
 
@@ -144,37 +108,44 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Conway's Game of Life</h1>
-        {ref===0?<ActiveLabel>I recommend you start with the Randomize <i className="fas fa-random" /> button below.</ActiveLabel>:<></>}
-        {ref===1?<><ActiveLabel>Click the cells to change their status.</ActiveLabel><ActiveLabel>Press the Play <i className="fas fa-play" /> button below when you're ready to watch your world develop.</ActiveLabel></>:<></>}
-        <Board>{rows.map(row=>{
-          return <CellRow>{row.map(cell=>cell.alive?<LiveCell key={`${cell.row},${cell.column}`} onClick={()=>flip(cell.row,cell.column)}></LiveCell>:<DeadCell key={`${cell.row},${cell.column}`} onClick={()=>flip(cell.row,cell.column)}></DeadCell>)}</CellRow>
-        })}</Board>
-        <ActiveIcon className="fas">Generation: {world.generation}</ActiveIcon>
-        <ButtonBox>
-          <Icon className="fas fa-undo" onClick={reset} title="Reset" />
-          {ref===0?<ActiveIcon className="fas fa-random" onClick={random} title="Randomize" />:<Icon className="fas fa-random" onClick={random} title="Randomize" />}
-          {isActive? <ActiveIcon className="fas fa-pause" onClick={toggle} title="Pause" />:ref===0?<Icon className="fas fa-play" onClick={toggle} title="Play" />:<ActiveIcon className="fas fa-play" onClick={toggle} title="Play" />}
-          <Icon className="fas fa-step-forward" onClick={step} title="Step Forward" />
-          <Icon className="fas fa-fast-forward" onClick={jump} title="Jump Forward">
-             {/* <NumberInput type="text" defaultValue="5"></NumberInput> */}
-           </Icon>
-        </ButtonBox>
-        <ButtonBox>
-          <ActiveIcon className="fas">Speed: </ActiveIcon>
-          {speed===1000?<ActiveIcon className="fas fa-caret-right" onClick={()=>setSpeed(1000)} title="1 Generation/Second" />:<Icon className="fas fa-caret-right" onClick={()=>setSpeed(1000)} title="1 Generation/Second" />}
-          {speed===500?<ActiveIcon className="fas fa-forward" onClick={()=>setSpeed(500)} title="2 Generations/Second" />:<Icon className="fas fa-forward" onClick={()=>setSpeed(500)} title="2 Generations/Second" />}
-          {speed===250?<ActiveIcon className="fas fa-forward" onClick={()=>setSpeed(250)} title="4 Generations/Second"><i className="fas fa-forward" /></ActiveIcon>:<Icon className="fas fa-forward" onClick={()=>setSpeed(250)} title="4 Generations/Second"><i className="fas fa-forward" /></Icon>}
-        </ButtonBox>
-        <ButtonBox>
-          {size===0?<ActiveIcon className="fas fa-expand" onClick={()=>resize(10,10,0)}> 10x10</ActiveIcon>:<Icon className="fas fa-expand" onClick={()=>resize(10,10,0)}> 10x10</Icon>}
-          {size===1?<ActiveIcon className="fas fa-expand" onClick={()=>resize(20,20,1)}> 20x20</ActiveIcon>:<Icon className="fas fa-expand" onClick={()=>resize(20,20,1)}> 20x20</Icon>}
-          {size===2?<ActiveIcon className="fas fa-expand" onClick={()=>resize(25,25,2)}> 25x25</ActiveIcon>:<Icon className="fas fa-expand" onClick={()=>resize(25,25,2)}> 25x25</Icon>}
-          {size===3?<ActiveIcon className="fas fa-expand" onClick={()=>resize(25,50,3)}> 25x50</ActiveIcon>:<Icon className="fas fa-expand" onClick={()=>resize(25,50,3)}> 25x50</Icon>}
-          {size===4?<ActiveIcon className="fas fa-expand" onClick={()=>resize(50,50,4)}> 50x50</ActiveIcon>:<Icon className="fas fa-expand" onClick={()=>resize(50,50,4)}> 50x50</Icon>}
-        </ButtonBox>
-      </header>
+        <h1 className="title bungee active strong">Conway's Game of Life</h1>
+        {showInfo?<div className="label active" onClick={()=>setShowInfo(false)}>{info}</div>:<div className="label" onClick={()=>setShowInfo(true)}>What is this? <i className="far fa-question-circle" /></div>}
+        {message===0?<div className="label-box" onClick={()=>setMessage(1)}><span className="label active">Click the cells to set their status or press the Randomize <i className="fas fa-random" /> button below.</span></div>:<></>}
+        {message===1?<div className="label-box"><span className="active">Click the cells to change their status.<br />Press the Play <i className="fas fa-play" /> button below when you're ready to watch your world develop.</span></div>:<></>}
+        {message>1?<div className="label-box" />:<></>}
+        <div className="button-box">
+          <i className={message===0?"fas fa-random active":"fas fa-random label"} onClick={random} title="Randomize" />
+          <i className="fas fa-undo label" onClick={reset} title="Reset" />
+          {isActive?<i className="fas fa-pause active" onClick={toggle} title="Pause" />:message===0?<i className="fas fa-play label" onClick={toggle} title="Play" />:<i className="fas fa-play active" onClick={toggle} title="Play" />}
+          <i className="fas fa-step-forward label" onClick={step} title="Step Forward" />
+          <i className="fas fa-fast-forward label" onClick={jump} title="Jump Forward" />
+        </div>
+        <div className="button-box">
+          <span className="fas orbit strong active">Speed: </span>
+          <span className="divider" />
+          <i className={speed===1000?"fas fa-caret-right active":"fas fa-caret-right label"} onClick={()=>setSpeed(1000)} title="1 Generation/Second" />
+          <span className="divider" />
+          <i className={speed===500?"fas fa-forward active":"fas fa-forward label"} onClick={()=>setSpeed(500)} title="2 Generations/Second" />
+          <span className="divider" />
+          <i className={speed===250?"fas fa-forward active":"fas fa-forward label"} onClick={()=>setSpeed(250)} title="4 Generations/Second"><i className="fas fa-forward" /></i>
+        </div>
+        <div className="button-box">
+          <span className="fas orbit active strong">Size: </span>
+          <span className="divider" />
+          <i className={size===0?"fas fa-expand active":"fas fa-expand label"} onClick={()=>resize(10,10,0)}> <span className="orbit">10x10</span></i>
+          <span className="divider" />
+          <i className={size===1?"fas fa-expand active":"fas fa-expand label"} onClick={()=>resize(20,20,1)}> <span className="orbit">20x20</span></i>
+          <span className="divider" />
+          <i className={size===2?"fas fa-expand active":"fas fa-expand label"} onClick={()=>resize(25,25,2)}> <span className="orbit">25x25</span></i>
+          <span className="divider" />
+          <i className={size===3?"fas fa-expand active":"fas fa-expand label"} onClick={()=>resize(25,50,3)}> <span className="orbit">25x50</span></i>
+          <span className="divider" />
+          <i className={size===4?"fas fa-expand active":"fas fa-expand label"} onClick={()=>resize(50,50,4)}> <span className="orbit">50x50</span></i>
+        </div>
+        <div className="board">{rows.map(row=>{
+          return <div className="cell-row">{row.map(cell=><div key={`${cell.row}-${cell.column}`} className="cell" onClick={()=>flip(cell.row,cell.column)}><div className={cell.alive?cellAliveClass:cellClass} /></div>)}</div>
+        })}</div>
+        <div className="italic active strong">Generation: {world.generation}</div>
     </div>
   );
 }
---
+++ src/info.js
@@ -0,0 +1,4 @@
+const info = "The Game of Life was invented in 1970 by British mathematician John Conway.\
+  Following simple rules, the cells on the board are born and die, creating intriquing patterns.";
+
+export {info};
